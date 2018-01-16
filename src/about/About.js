1289daec67b55f4df962d7b55587b01029830d42
Jescy Querimit <jezkie00secondary@gmail.com>
2018-01-16 16:29:11 +0800

    Breadcrumbs

--- src/about/About.js
+++ src/about/About.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Menu, Container } from 'semantic-ui-react';
+import { Menu, Container, Breadcrumb, Grid } from 'semantic-ui-react';
 import { ABOUT_MENUS } from '../const/menus';
 import { Link } from 'react-router-dom';
 
@@ -14,23 +14,51 @@ class About extends Component {
     // constructor(props) {
     //     super(props);
     // }
+    getDisplayName(clickedMenu) {
+        let display =
+            ABOUT_MENUS.map((item, i) => {
+                if (item.name === clickedMenu) {
+                    return item.display;
+                } else {
+                    return null;
+                }
+            })
+        return display;
+    }
+
     render() {
         return (
             <div>
-                <Menu fixed='top' size='large'>
-                    <Container>
-                        {
-                            ABOUT_MENUS.map((item, i) => {
-                                return (
-                                    <Menu.Item as={Link} to={item.link}
-                                        active={item.name === this.props.clickedMenu}
-                                        key={item.name}>{item.display}
-                                    </Menu.Item>
-                                )
-                            })
-                        }
-                    </Container>
-                </Menu>
+                <Grid container stackable verticalAlign='middle'>
+                    <Grid.Row>
+                        <Menu size='large'>
+                            <Container>
+                                {
+                                    ABOUT_MENUS.map((item, i) => {
+                                        return (
+                                            <Menu.Item as={Link} to={item.link}
+                                                active={item.name === this.props.clickedMenu}
+                                                key={item.name}>{item.display}
+                                            </Menu.Item>
+                                        )
+                                    })
+                                }
+                            </Container>
+                        </Menu>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Breadcrumb>
+                            <Container>
+                                <Breadcrumb.Section link as={Link} to='/'>Home</Breadcrumb.Section>
+                                <Breadcrumb.Divider icon='right angle'></Breadcrumb.Divider>
+                                <Breadcrumb.Section link as={Link} to='/about'>About</Breadcrumb.Section>
+                                <Breadcrumb.Divider icon='right angle'></Breadcrumb.Divider>
+                                <Breadcrumb.Section active>{this.getDisplayName(this.props.clickedMenu)}</Breadcrumb.Section>
+                            </Container>
+                        </Breadcrumb>
+                    </Grid.Row>
+                </Grid>
+
                 {this.props.children}
             </div>
         );
--- src/join/JoinUs.js
+++ src/join/JoinUs.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Menu, Container } from 'semantic-ui-react';
+import { Menu, Container, Breadcrumb, Grid } from 'semantic-ui-react';
 import { JOIN_MENUS } from '../const/menus'
 import { Link } from 'react-router-dom';
 
@@ -7,24 +7,50 @@ class JoinUs extends Component {
     // constructor(props) {
     //     super(props);
     // }
-
+    getDisplayName(clickedMenu) {
+        let display =
+            JOIN_MENUS.map((item, i) => {
+                if (item.name === clickedMenu) {
+                    return item.display;
+                } else {
+                    return null;
+                }
+            })
+        return display;
+    }
     render() {
         return (
             <div>
-                <Menu fixed='top' size='large'>
-                    <Container>
-                        {
-                            JOIN_MENUS.map((item, i) => {
-                                return (
-                                    <Menu.Item as={ Link } to={item.link} 
-                                        active={item.name === this.props.clickedMenu} 
-                                        key={item.name}>{item.display}
-                                    </Menu.Item>
-                                )
-                            })
-                        }
-                    </Container>
-                </Menu>
+                <Grid container stackable verticalAlign='middle'>
+                    <Grid.Row>
+                        <Menu size='large'>
+                            <Container>
+                                {
+                                    JOIN_MENUS.map((item, i) => {
+                                        return (
+                                            <Menu.Item as={Link} to={item.link}
+                                                active={item.name === this.props.clickedMenu}
+                                                key={item.name}>{item.display}
+                                            </Menu.Item>
+                                        )
+                                    })
+                                }
+                            </Container>
+                        </Menu>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Breadcrumb>
+                            <Container>
+                                <Breadcrumb.Section link as={Link} to='/'>Home</Breadcrumb.Section>
+                                <Breadcrumb.Divider icon='right angle'></Breadcrumb.Divider>
+                                <Breadcrumb.Section link as={Link} to='/join'>Join Us</Breadcrumb.Section>
+                                <Breadcrumb.Divider icon='right angle'></Breadcrumb.Divider>
+                                <Breadcrumb.Section active>{this.getDisplayName(this.props.clickedMenu)}</Breadcrumb.Section>
+                            </Container>
+                        </Breadcrumb>
+                    </Grid.Row>
+                </Grid>
+
                 {this.props.children}
             </div>
         );
